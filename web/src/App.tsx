ed5efa700a91bf68867f9fd7672d16828a46673e
sang <svngdo@gmail.com>
2026-06-18 23:16:20 +0700

    feat(web): add layout and routing
    - Add MainLayout and Sidebar components
    - Add application pages and routes
    - Configure react-router-dom
    - Configure Tailwind CSS
    - Enable absolute path imports

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-  return <>Viral Utils</>;
-}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "./components/layout/MainLayout";
+import { routes } from "./routes";
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <MainLayout>
+        <Routes>
+          {routes.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+      </MainLayout>
+    </BrowserRouter>
+  );
+}
