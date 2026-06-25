af6cfc04feeff9a317b08ff52b3770bffad2e2ac
sang <svngdo@gmail.com>
2026-06-25 08:55:26 +0700

    fix tikhub default parameters and exception handler

--- api/src/tikhub/constants.py
+++ api/src/tikhub/constants.py
@@ -2,6 +2,7 @@ from src.config import settings
 
 TIKHUB_BASE_URL = "https://api.tikhub.io/api/v1/douyin/app/v3"
 DOUIYIN_USER_BASE_URL = "https://www.douyin.com/user"
+DEFAULT_USER_POST_VIDEO_COUNT = 20
 DEFAULT_HEADERS = {
     "Accept": "application/json",
     "Authorization": f"Bearer {settings.tikhub_auth_token}",
